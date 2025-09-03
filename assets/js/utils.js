64a5b6e471d7be049e7858eb3aaec725d87a2b38
Jose Ramón Caravaca <joseramoncaravaca@gmail.com>
2025-09-03 06:56:23 +0200

    feat(addBook): add book functionality

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -8,4 +8,58 @@ function addBookToLibrary(arr) {
     arr.push(newBook); 
 }; 
 
-export {addBookToLibrary}; 
+function paintBooksDOM(arr) {
+    for (let i = 0; i < arr.length; i++) {
+    let book = arr[i]; 
+    let newCard = document.createElement("div"); 
+    newCard.classList.add("book-card"); 
+    let title = newCard.appendChild(document.createElement("p")); 
+    let author = newCard.appendChild(document.createElement("p")); 
+    let pages = newCard.appendChild(document.createElement("p"));  
+    const bookContainer = document.getElementById("book-container")
+
+    let readButton = newCard.appendChild(document.createElement("button"));
+    readButton.classList.add("read-button"); 
+    
+    if (book.read === true) {
+            readButton.style.backgroundColor = "#6DFF89"; 
+            readButton.textContent = "✅ Read"
+    } else {
+            readButton.style.backgroundColor = "#FF676A"; 
+            readButton.textContent = "❌ Not Read";
+    }
+
+    readButton.addEventListener("click", (event) => {
+        event.preventDefault(); 
+        
+        if (book.read === true) {
+            book.read = false;
+            readButton.style.backgroundColor = "#FF676A"; 
+            readButton.textContent = "❌ Not Read";
+        } else {
+            book.read = true;
+            readButton.style.backgroundColor = "#6DFF89"; 
+            readButton.textContent = "✅ Read"
+        }
+    })
+
+    let remove = newCard.appendChild(document.createElement("button")); 
+    remove.classList.add("remove-button")
+    remove.textContent = "Remove Book";
+    remove.addEventListener("click", (event) => {
+        event.preventDefault();
+        arr.splice(i,1); 
+        bookContainer.removeChild(newCard); 
+    }); 
+
+
+
+    title.textContent = `Title: ${book.title}`; 
+    author.textContent = `Author: ${book.author}`; 
+    pages.textContent = `Pages: ${book.pages}`; 
+
+    bookContainer.appendChild(newCard); 
+}
+}
+
+export {addBookToLibrary, paintBooksDOM}; 
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import {Book} from "./assets/js/classes.js"; 
-import { addBookToLibrary } from "./assets/js/utils.js";
+import { addBookToLibrary, paintBooksDOM } from "./assets/js/utils.js";
 
 const book1 = new Book("Radiant Words", "Brandon Sanderson", 1248); 
 const book2 = new Book("Oathbringer", "Brandon Sanderson", 1408);
@@ -7,55 +7,37 @@ const book2 = new Book("Oathbringer", "Brandon Sanderson", 1408);
 const myLibrary = [book1, book2]; 
 const bookContainer = document.getElementById("book-container")
 
-// Bucle para pintar los libros en el DOM con sus funcionalidades
-for (let i = 0; i < myLibrary.length; i++) {
-    let book = myLibrary[i]; 
-    let newCard = document.createElement("div"); 
-    newCard.classList.add("book-card"); 
-    let title = newCard.appendChild(document.createElement("p")); 
-    let author = newCard.appendChild(document.createElement("p")); 
-    let pages = newCard.appendChild(document.createElement("p"));  
+const newBookButton = document.getElementById("add-book"); 
+const dialog = document.getElementById("dialog"); 
 
-    let readButton = newCard.appendChild(document.createElement("button"));
-    readButton.classList.add("read-button"); 
+newBookButton.addEventListener("click", (event) => {
+    event.preventDefault(); 
+    dialog.show(); 
     
-    if (book.read === true) {
-            readButton.style.backgroundColor = "#6DFF89"; 
-            readButton.textContent = "✅ Read"
-    } else {
-            readButton.style.backgroundColor = "#FF676A"; 
-            readButton.textContent = "❌ Not Read";
-    }
+})
 
-    readButton.addEventListener("click", (event) => {
-        event.preventDefault(); 
-        
-        if (book.read === true) {
-            book.read = false;
-            readButton.style.backgroundColor = "#FF676A"; 
-            readButton.textContent = "❌ Not Read";
-        } else {
-            book.read = true;
-            readButton.style.backgroundColor = "#6DFF89"; 
-            readButton.textContent = "✅ Read"
-        }
-    })
+const addBookButton = document.getElementById('new-book-add'); 
+addBookButton.addEventListener('click', (event) => {
+    event.preventDefault(); 
+    let title = document.getElementById("new-book-title").value; 
+    let author = document.getElementById("new-book-author").value; 
+    let pages = document.getElementById("new-book-pages").value; 
 
-    let remove = newCard.appendChild(document.createElement("button")); 
-    remove.classList.add("remove-button")
-    remove.textContent = "Remove Book";
-    remove.addEventListener("click", (event) => {
-        event.preventDefault();
-        myLibrary.splice(i,1); 
-        bookContainer.removeChild(newCard); 
-    }); 
+    const book = new Book(title, author, pages); 
+
+    myLibrary.push(book); 
+    bookContainer.innerHTML = ""; 
+    paintBooksDOM(myLibrary); 
+    dialog.close(); 
+}); 
+
+const closeDialog = document.getElementById("close-dialog"); 
+
+closeDialog.addEventListener("click", (event) => {
+    dialog.close(); 
+})
 
 
-
-    title.textContent = `Title: ${book.title}`; 
-    author.textContent = `Author: ${book.author}`; 
-    pages.textContent = `Pages: ${book.pages}`; 
-
-    bookContainer.appendChild(newCard); 
-}
+// Bucle para pintar los libros en el DOM con sus funcionalidades
+paintBooksDOM(myLibrary); 
 
